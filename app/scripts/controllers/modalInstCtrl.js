30b03bb1c31ba3501144aea12eb5404a7f5f78f0
ChamuKallem <chamukallem@gmail.com>
2017-01-01 17:50:31 -0800

    BlocChat with ExtraCredit-1

---
+++ app/scripts/controllers/modalInstCtrl.js
@@ -0,0 +1,22 @@
+(function(){
+    function modalInstCtrl(roomService, $uibModalInstance, $scope){
+        var $ctrl = this;
+    
+        $ctrl.focus = function(){
+          $$uibModalInstance.focus();
+        };
+        $ctrl.ok = function () {
+            $uibModalInstance.close();
+            var newRoomName = $scope.newRoomName;
+            var newRoomCreatedOn = new Date().getDate();
+            roomService.addRoom(newRoomName,newRoomCreatedOn);
+        };
+  
+        $ctrl.cancel = function () {
+          $uibModalInstance.dismiss('cancel');
+        };   
+    }
+    angular
+        .module('Bloc-Chat')
+        .controller('modalInstCtrl', ['roomService', '$uibModalInstance', '$scope', modalInstCtrl])
+})();
